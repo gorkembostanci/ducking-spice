fa563d20c6bb1d7fd60072195513dfaff9434852
gbostanci <gbostanci13@ku.edu.tr>
2014-10-26 15:13:50 +0200

    prog assg 2
    Programming Assignment 2 for R Programming course

---
+++ cachematrix.R
@@ -0,0 +1,38 @@
+## These two functions take an invertible matrix and compute its inverse. Using
+## their caching ability, they do not need to compute the inverse of the same 
+## matrix again and again.
+
+## This function creates a list which contains information about the matrix and 
+## its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+}
+
+
+## This function inputs the output of makeCacheMatrix and evaluates and outputs
+## the inverse of the matrix
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
